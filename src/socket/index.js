4d4199b862fcae361c0cc715d3b6e2b13840ed84
navatar <umdomby@yandex.ru>
2022-03-02 22:50:04 +0300

    first commit

---
+++ src/socket/index.js
@@ -0,0 +1,13 @@
+import {io} from 'socket.io-client';
+
+const options = {
+  "force new connection": true,
+  reconnectionAttempts: "Infinity", // avoid having user reconnect manually in order to prevent dead clients after a server restart
+  timeout : 10000, // before connect_error and connect_timeout are emitted.
+  transports : ["websocket"]
+}
+
+//const socket = io('/', options);
+const socket = io('https://umdom.by:4433', options);
+
+export default socket;
