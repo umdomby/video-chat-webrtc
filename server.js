e761df0a7016a6202c4ea7d0ddf98e63d3721cf2
umdomby <umdomby@yandex.ru>
2022-10-07 17:03:21 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,118 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+//const server = require('http').createServer(app);
+//const io = require('socket.io')(server);
+const {version, validate} = require('uuid');
+
+const ACTIONS = require('./src/socket/actions');
+//const PORT = process.env.PORT || 3001;
+const fs = require("fs");
+const privateKey = fs.readFileSync(path.resolve(__dirname,'./cert/servicerobotpro/privkey.pem'));
+const certificate = fs.readFileSync(path.resolve(__dirname,'./cert/servicerobotpro/cert.pem'));
+const ca = fs.readFileSync(path.resolve(__dirname,'./cert/servicerobotpro/chain.pem'));
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca
+};
+const https = require('https');
+const httpsServer = https.createServer(credentials, app);
+const io = require('socket.io')(httpsServer);
+
+function getClientRooms() {
+  const {rooms} = io.sockets.adapter;
+
+  return Array.from(rooms.keys()).filter(roomID => validate(roomID) && version(roomID) === 4);
+}
+
+function shareRoomsInfo() {
+  io.emit(ACTIONS.SHARE_ROOMS, {
+    rooms: getClientRooms()
+  })
+}
+
+io.on('connection', socket => {
+  shareRoomsInfo();
+
+  socket.on(ACTIONS.JOIN, config => {
+    const {room: roomID} = config;
+    const {rooms: joinedRooms} = socket;
+
+    if (Array.from(joinedRooms).includes(roomID)) {
+      return console.warn(`Already joined to ${roomID}`);
+    }
+
+    const clients = Array.from(io.sockets.adapter.rooms.get(roomID) || []);
+
+    clients.forEach(clientID => {
+      io.to(clientID).emit(ACTIONS.ADD_PEER, {
+        peerID: socket.id,
+        createOffer: false
+      });
+
+      socket.emit(ACTIONS.ADD_PEER, {
+        peerID: clientID,
+        createOffer: true,
+      });
+    });
+
+    socket.join(roomID);
+    shareRoomsInfo();
+  });
+
+  function leaveRoom() {
+    const {rooms} = socket;
+
+    Array.from(rooms)
+      // LEAVE ONLY CLIENT CREATED ROOM
+      .filter(roomID => validate(roomID) && version(roomID) === 4)
+      .forEach(roomID => {
+
+        const clients = Array.from(io.sockets.adapter.rooms.get(roomID) || []);
+
+        clients
+          .forEach(clientID => {
+          io.to(clientID).emit(ACTIONS.REMOVE_PEER, {
+            peerID: socket.id,
+          });
+
+          socket.emit(ACTIONS.REMOVE_PEER, {
+            peerID: clientID,
+          });
+        });
+
+        socket.leave(roomID);
+      });
+
+    shareRoomsInfo();
+  }
+
+  socket.on(ACTIONS.LEAVE, leaveRoom);
+  socket.on('disconnecting', leaveRoom);
+
+  socket.on(ACTIONS.RELAY_SDP, ({peerID, sessionDescription}) => {
+    io.to(peerID).emit(ACTIONS.SESSION_DESCRIPTION, {
+      peerID: socket.id,
+      sessionDescription,
+    });
+  });
+
+  socket.on(ACTIONS.RELAY_ICE, ({peerID, iceCandidate}) => {
+    io.to(peerID).emit(ACTIONS.ICE_CANDIDATE, {
+      peerID: socket.id,
+      iceCandidate,
+    });
+  });
+
+});
+
+// app.use(express.static('./client/build'));
+//
+// app.get('*', (req,res)=>{
+//     res.sendFile(path.resolve(__dirname, "client","build","index.html"));
+// })
+
+httpsServer.listen(4444, () => {
+  console.log('HTTPS Server running on port 4444');
+});
